fcaca2d3334b9faed248947b59260fe0122e0868
Paweł Lampe <scony@scony-netbook>
2013-10-26 20:42:03 +0200

    Db structure

---
+++ sql/db.sql
@@ -0,0 +1,18 @@
+create database webify;
+
+create table recipes (
+       id int not null auto_increment primary key,
+       name varchar(64) not null,
+       description blob,
+       add_ts bigint not null,
+       url varchar(255) not null
+       -- feature list
+       -- params
+);
+
+create table rates (
+       id int not null auto_increment primary key,
+       rid int not null references recipes(id),
+       rate tinyint not null,
+       ip varchar(15) not null
+);
