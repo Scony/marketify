b50d1263cb94c81bdd7b735ec21e5df2caf645a5
Scony <scony@sconysoft.com>
2013-12-22 14:27:02 +0100

    mock fix

--- mock/handler.php
+++ mock/handler.php
@@ -1,29 +1,31 @@
 <?php
 require_once('../app/config.php');
 
-$users = array('scony','alx','mateusz','fisz','patryk','root');
+$users = array('scony','alx','mateusz','fisz','patryk','root','alxio');
 $in = file_get_contents('php://input');
 
 if(!empty($in))
   {
     $arr = json_decode($in,true);
+    $recipe = $arr['user']['recipe'];
+    $group = $arr['user']['group'];
+    $user = $arr['user']['username'];
 
     if($arr['event']['tag'] != -4)
       {
 	if($arr['event']['target'] == 'BROADCAST')
 	  foreach($users as $user)
-	    Db::b('insert into mock values(NULL,?,?,?)','ssi',array($user,$in,time()));
+	    Db::b('insert into mock values(NULL,?,?,?,?,?)','ssssi',array($recipe,$group,$user,$in,time()));
 	else
 	  {
-	    $tmp = explode('@',$arr['event']['target']);
-	    Db::b('insert into mock values(NULL,?,?,?)','ssi',array($tmp[0],$in,time()));
+	    Db::b('insert into mock values(NULL,?,?,?,?,?)','ssssi',array($recipe,$group,$arr['event']['target'],$in,time()));
 	  }
       }
     else
       {
-	if(Db::n('select 1 from mock where target="'.$arr['user']['username'].'"'))
+	if(Db::n('select 1 from mock where target="'.$user.'" and team="'.$group.'" and recipe="'.$recipe.'"'))
 	  {
-	    $re = Db::b('select * from mock where target=? order by id desc limit 1','s',array($arr['user']['username']));
+	    $re = Db::b('select * from mock where target=? and team=? and recipe=? order by id desc limit 1','sss',array($user,$group,$recipe));
 	    echo $re[0]['json'];
 	    Db::b('delete from mock where id=?','i',array($re[0]['id']));
 	  }
